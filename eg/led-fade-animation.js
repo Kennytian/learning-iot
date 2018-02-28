00df9fb914c1f12b118a545417e9211417bbb4f0
Kennytian <kenny_tian@outlook.com>
2018-02-28 21:22:17 +0800

    Add 2 js files

---
+++ eg/led-fade-animation.js
@@ -0,0 +1,15 @@
+const {
+  Board,
+  Led
+} = require('johnny-five');
+new Board().on('ready', () => {
+  const led = new Led(11);
+  led.fade({
+    easing: 'linear',
+    duration: 1000,
+    cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
+    keyFrames: [0, 250, 25, 150, 100, 125],
+    onstop: () => console.log('Animation stopped.'),
+  });
+  this.wait(2000, () => led.fadeOut());
+});
---
+++ eg/led-fade-callback.js
@@ -0,0 +1,30 @@
+const {
+  Board,
+  Leds
+} = require('johnny-five');
+new Board().on('ready', () => {
+  const leds = new Leds([11, 10, 9, 6, 5, 3]);
+  const timing = 250;
+  const randomFade = true;
+  let fadeIndex = 0;
+  const ledCount = leds.length;
+
+  function fadeNext(){
+    let candidateIndex = fadeIndex;
+    leds[fadeIndex].fadeIn(timing);
+
+    // Determine the next LED to fade
+    if(randomFade){
+      while(candidateIndex===fadeIndex){
+        candidateIndex = Math.round(Math.random()*(ledCount-1));
+      }
+    }else{
+      candidateIndex = (fadeIndex<ledCount -1)?fadeIndex+1:0;
+    }
+    fadeIndex = candidateIndex;
+    leds[fadeIndex].fadeOut(timing, fadeNext);
+  }
+
+  leds.on();
+  leds[fadeIndex].fadeOut(timing, fadeNext);
+});
